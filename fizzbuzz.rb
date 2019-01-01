d15314c9edda8663df85372ad88b420682cde4b8
Joshua M Loran <joshua.loran@gmail.com>
2019-01-01 01:14:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
-  if3 = "Fizz"
-  if5 = "Buzz"
   if (int % 3 == 0 && int % 5 == 0)
-    reply = if3 + if5
-    reply
+    "FizzBuzz"
   elsif int % 3 == 0
-    if3
+    "Fizz"
   elsif int % 5 == 0
-    if5
+    "Buzz"
   else
   end
 end
