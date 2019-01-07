b97ddb2ce70689d8dd0ef1851437ee1a786e3fc3
Isa Levine <isaacdlevine@gmail.com>
2019-01-07 05:27:07 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
+def fizzbuzz(numnum, langlang = "")
+  if numnum % 3 == 0 && numnum % 5 == 0
+    langlang = "FizzBuzz"
+  elsif numnum % 3 == 0
+    langlang << "Fizz"
+  elsif numnum % 5 == 0
+    langlang = "Buzz"
+  end
+end
+
+
+
+
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
