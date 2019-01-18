a1443b821fd74e0c33e7ed0601a3ca4d9ec5eb99
Njenga Kariuki <njengak@Njengas-MacBook-Pro-2.local>
2019-01-18 14:53:34 -0800

    rspec fizzbuzz lab

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  case
+  when (num % 3 == 0 && num % 5 ==0)then "FizzBuzz"
+    when num % 3 == 0 then "Fizz"
+    when num % 5 == 0  then "Buzz"
+  end
+end
