65cd8fef9307c554df8a146cde11448aa24c20a8
Kim Heidorn <heidorkn@gmail.com>
2019-02-07 23:27:05 +0000

    Done.

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(num)
+  num_3 = (num%3 == 0)
+  num_5 = (num%5 == 0)
+  if num_3&&num_5
+    "FizzBuzz"
+  elsif num_5
+    "Buzz"
+  elsif num_3
+    "Fizz"
+  else 
+    nil
+  end
+end
